74918120bb723b5809228694aca45826b8123a09
macbook <nagelbergmark@gmail.com>
2016-10-06 11:12:52 -0230

    First working version that sends bare bones email

--- build-email.py
+++ build-email.py
@@ -1,6 +1,10 @@
 from jinja2 import Environment, FileSystemLoader
 import datetime
 from transaction_analyzer import TransactionData
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import json
 
 env = Environment(loader=FileSystemLoader('templates'))
 template = env.get_template('email.html')
@@ -20,11 +24,30 @@ if last_month == 0:
     last_month = 12
     last_month_year = last_month_year - 1
 
+
 last_month_transactions.filter_dates(begin_datetime = datetime.date(day = 1, month = last_month, year = last_month_year))
 
-last_month_transactions = TransactionData("transactions.csv")
 template_context["last_month_after_tax_income"] = last_month_transactions.after_tax_income()
 template_context["last_month_spending"] = last_month_transactions.spending()
 template_context["last_month_savings_rate"] = last_month_transactions.savings_rate()
 
-print template.render(template_context)
+html  =  template.render(template_context)
+
+email_info = json.load(open('emails.json'))
+sender = email_info["sender"]
+receivers = email_info["receivers"]
+
+for receiver in receivers:
+    msg = MIMEMultipart('alternative')
+    msg['Subject'] = "Mint Financial Independence Report"
+    msg['From'] = sender
+    msg['To'] = receiver
+    msg.attach(MIMEText(html, 'html'))
+
+    s = smtplib.SMTP_SSL(host = 'smtp.gmail.com', port = 465)
+    email_login_info = json.load(open('email_user_pass.json'))
+    s.login(user = email_login_info['username'], password = email_login_info['password'])
+
+    s.sendmail(sender, receiver, msg.as_string())
+
+    s.quit()
